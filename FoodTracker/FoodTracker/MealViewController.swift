00b5ad850248c1e36d4cb1dc6cda91f3c3741627
doguico <guido.corazza@outlook.com>
2017-08-22 17:19:05 -0300

    Added behavior to the add button: enable and disable according to the meal name

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -24,10 +24,19 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
 
         // Handle the text field’s user input through delegate callbacks.
         mealNameTextField.delegate = self
+        
+        // Enable the Save button only if the text field has a valid Meal name.
+        updateSaveButtonState()
     }
     
 
     //MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.isEnabled = false
+    }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //Hide keyboard
         mealNameTextField.resignFirstResponder()
@@ -35,7 +44,8 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     }
     
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
-
+        updateSaveButtonState()
+        navigationItem.title = textField.text
     }
     
     //MARK: UIImagePickerControllerDelegate
@@ -94,6 +104,15 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
         
+    }
+    
+    //MARK: Private Methods
+    
+    private func updateSaveButtonState() {
+        // Disable the Save button if the text field is empty.
+
+        let text = mealNameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
     }
 }
 
