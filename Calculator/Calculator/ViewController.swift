6e85c3fcce3881513afe4976be1fe3bf81e1750c
doguico <guido.corazza@outlook.com>
2017-09-03 16:01:46 -0300

    Adding logic to calculator

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfWritingANumber = false
+    var digits = [Double]()
+    
+    @IBAction func appendDigit(_ sender: UIButton) {
+        if userIsInTheMiddleOfWritingANumber {
+            display.text = display.text! + sender.currentTitle!
+        }
+        else{
+            userIsInTheMiddleOfWritingANumber = true
+            display.text = sender.currentTitle!
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enter() {
+        let digit = Double(display.text!)
+        digits.append(digit!)
+        userIsInTheMiddleOfWritingANumber = false
+        print("\(digits)")
     }
-
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfWritingANumber{
+            enter()
+        }
+        
+        switch operation{
+        case "+": operate { $0 + $1 }
+        case "−": operate { $1 - $0 }
+        case "×": operate { $0 * $1 }
+        case "÷": operate { $1 / $0 }
+        case "√": operateSingleParameter { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func operate(operation: (Double, Double) -> Double){
+        if digits.count >= 2{
+            let newValue = operation(digits.removeLast(), digits.removeLast())
+            display.text = "\(newValue)"
+            enter()
+        }
+    }
+    
+    func operateSingleParameter(operation: (Double) -> Double){
+        if digits.count >= 1{
+            let newValue = operation(digits.removeLast())
+            display.text = "\(newValue)"
+            enter()
+        }
+    }
+    
 }
 
